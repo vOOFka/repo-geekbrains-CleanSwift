c807175acb67acaba7fd7399bdde7d47f26b86e2
Vladimir Sirel <exit551@ya.ru>
2022-02-12 11:09:46 +0300

    Initial commit

---
+++ shopVS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopVS
+//
+//  Created by Home on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
