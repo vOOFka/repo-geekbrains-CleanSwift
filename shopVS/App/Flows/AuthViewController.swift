3cccea7b90de2efc3ae2b9b0658043897b0f3dbe
Vladimir Sirel <exit551@ya.ru>
2022-03-23 23:57:45 +0300

    change authvc layout

--- shopVS/App/Flows/AuthViewController.swift
+++ shopVS/App/Flows/AuthViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+final class AuthViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
     
+    // MARK: - Properties
     private let requestFactory = RequestFactory()
     private let currentUser = User(id: 0140828713151909195,
                                    login: "exit551",
@@ -17,20 +23,61 @@ class AuthViewController: UIViewController {
                                                             lastname: "Sirel",
                                                             email: "exit551@ya.ru",
                                                             creditCard: "1234-5678-9101-0000"))
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
         
-        //authRequest()
-        //logoutRequest()
-        //signUpRequest()
-        //editProfileRequest(currentUser)
-        //getGoodsRequest(pageNumber: 1, categoryId: 321)
-        //getProductRequest(productId: 111)
-        //getFeedbacksRequest(pageNumber: 11, productId: 111)
-        //addFeedbackRequest(productId: 111, newFeedback: Feedback(id: 0, userId: 23525, comment: "Example comment..."))
-        //removeFeedbackRequest(productId: 111, feedbackId: 444)
-        shoping()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        registerKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        removeNotifications()
+    }
+    
+    // MARK: - Methods
+    private func configUI() {
         
+    }
+    
+    private func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    private func removeNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func hideKeyboardGestre () {
+        let hideKeyboardGestre = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(hideKeyboardGestre)
+    }
+
+    @objc private func keyboardShow (_ notification: NSNotification) {
+        let userInfo = notification.userInfo
+        let keyboardSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let contentInsert = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height + 10, right: 0)
+        
+        scrollView.contentInset = contentInsert
+        scrollView.scrollIndicatorInsets = contentInsert
+        scrollView.scrollRectToVisible(enterButton.frame, animated: true)
+    }
+    
+    @objc private func keyboardHide () {
+        scrollView.contentInset = .zero
+    }
+    
+    @objc private func hideKeyboard () {
+        scrollView.endEditing(true)
     }
     
     private func authRequest() {
@@ -46,139 +93,11 @@ class AuthViewController: UIViewController {
         }
     }
     
-    private func logoutRequest() {
-        let auth = requestFactory.makeAuthRequestFactory()
-        
-        auth.logout(userId: 2361194105334859860) { response in
-            switch response.result {
-            case .success(let logout):
-                print(logout)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
+    // MARK: - Actions
+    
+    @IBAction private func signUpButtonTap(_ sender: Any) {
     }
     
-    private func signUpRequest() {
-        let profile = requestFactory.makeProfileRequestFactory()
-        
-        let newProfile = UserProfile(name: "Vladimir", lastname: "Sirel" , email: "exit551@ya.ru", creditCard: "1234-5678-9101-1121")
-        let newUser = User(id: 0, login: "exit551", password: "dasd123asd", userProfile: newProfile)
-        
-        
-        profile.signUp(user: newUser) { response in
-            switch response.result {
-            case .success(let signUp):
-                print(newUser)
-                print(signUp)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func editProfileRequest(_ someUser: User) {
-        let profile = requestFactory.makeProfileRequestFactory()
-        
-        profile.editProfile(user: someUser) { response in
-            switch response.result {
-            case .success(let editUser):
-                print(someUser)
-                print(editUser)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func getGoodsRequest(pageNumber: Int, categoryId: Int) -> [Product] {
-        let goods = requestFactory.makeGoodsRequestFactory()
-        var products: [Product] = []
-        
-        goods.getCatalogData(pageNumber: pageNumber, categoryId: categoryId) { response in
-            switch response.result {
-            case .success(let goods):
-                products = goods.goods ?? []
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-        return products
-    }
-    
-    private func getProductRequest(productId: Int) {
-        let goods = requestFactory.makeGoodsRequestFactory()
-        
-        goods.getProduct(productId: productId) { response in
-            switch response.result {
-            case .success(let product):
-                print(product)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func getFeedbacksRequest(pageNumber: Int, productId: Int) {
-        let feedbacks = requestFactory.makeFeedbacksRequestFactory()
-        
-        feedbacks.getFeedbacks(pageNumber: pageNumber, productId: productId) { response in
-            switch response.result {
-            case .success(let feedbacks):
-                print(feedbacks)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func addFeedbackRequest(productId: Int, newFeedback: Feedback) {
-        let feedbacks = requestFactory.makeFeedbacksRequestFactory()
-        
-        feedbacks.addFeedback(productId: productId, newFeedback: newFeedback) { response in
-            switch response.result {
-            case .success(let feedbacks):
-                print(feedbacks)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func removeFeedbackRequest(productId: Int, feedbackId: Int) {
-        let feedbacks = requestFactory.makeFeedbacksRequestFactory()
-        
-        feedbacks.removeFeedback(productId: productId, feedbackId: feedbackId) { response in
-            switch response.result {
-            case .success(let feedbacks):
-                print(feedbacks)
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-    
-    private func shoping() {
-        let basket = requestFactory.makeBasketRequestFactory()
-                
-        let productFirst = Product(id: 111, name: "Notebook ASUS", price: 3000, description: "Super fast notebook")
-        let productSecond = Product(id: 222, name: "Iphone", price: 2000, description: "Great phone")
-        UserBasket.shared.addProduct(productFirst)
-        UserBasket.shared.addProduct(productSecond)
-        
-        let allProducts = UserBasket.shared.products
-                
-        if !allProducts.isEmpty {
-            basket.payBasket(userId: 123) { response in
-                switch response.result {
-                case .success(let basketResult):
-                    print(basketResult)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        UserBasket.shared.clearProducts()
+    @IBAction private func enterButtonTap(_ sender: Any) {
     }
 }
-
