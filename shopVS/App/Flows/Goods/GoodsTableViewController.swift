a44be80cdc81b1c2aaef2301dff00e05174bc167
Vladimir Sirel <exit551@ya.ru>
2022-03-30 22:18:13 +0300

    added goods tvc, vm

--- shopVS/App/Flows/Goods/GoodsTableViewController.swift
+++ shopVS/App/Flows/Goods/GoodsTableViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-class GoodsTableViewController: UITableViewController {
+class GoodsTableViewController: UITableViewController {    
+    var viewModel: GoodsViewModel? {
+        didSet {
+            viewModel?.update(pageNumber: -1, categoryId: 123) { [weak self] in
+                self?.tableView.reloadData()
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,11 +22,47 @@ class GoodsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        viewModel?.cellsArray.count ?? 0
     }
 }
+
+
+final class GoodsViewModel {
+    private let requestFactory = RequestFactory()
+    private(set) var cellsArray: [GoodsViewCellModel] = []
+    
+    func update(pageNumber: Int, categoryId: Int, completion: @escaping () -> Void) {
+        let goods = requestFactory.makeGoodsRequestFactory()
+        
+        goods.getCatalogData(pageNumber: pageNumber, categoryId: categoryId) { [weak self] response in
+            guard let self = self else { return }
+            switch response.result {
+            case .success(let catalogResult):
+                DispatchQueue.main.async {
+                    if catalogResult.result == 1,
+                       let goods = catalogResult.goods {
+                       //TODO
+                        completion()
+                    } else {
+                        //TODO
+                        //self.showError(message: catalogResult.errorMessage ?? "Unknow error, please try again later.")
+                    }
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    //TODO
+                    //self.showError(message: error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+
+}
+
+
+final class GoodsViewCellModel {
+    
+}
+
---
+++ shopVS/App/Flows/Goods/GoodsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GoodsViewModel.swift
+//  shopVS
+//
+//  Created by Home on 30.03.2022.
+//
+
+import Foundation
