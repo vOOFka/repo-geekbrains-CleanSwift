7c763e11a2455c14e70a0907f976c98f0a4a7165
Vladimir Sirel <exit551@ya.ru>
2022-04-09 15:35:45 +0300

    added UITests authorization

--- shopVSUITests/shopVSUITests.swift
+++ shopVSUITests/shopVSUITests.swift
@@ -8,35 +8,53 @@
 import XCTest
 
 class shopVSUITests: XCTestCase {
+    var app: XCUIApplication!
+    var scrollViewsQuery: XCUIElementQuery!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app = XCUIApplication()
+        app.launch()
+        scrollViewsQuery = app.scrollViews
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+        scrollViewsQuery = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    private func deleteTextIn(_ textField: XCUIElement) {
+        while !(textField.value as! String).isEmpty {
+            let deleteKey = XCUIApplication().keys["delete"]
+            deleteKey.tap()
         }
+    }
+    
+    private func enterAuthorizationData(login: String, password: String) {
+        let loginTextField = scrollViewsQuery.textFields["loginTextField"].firstMatch
+        let passwordTextField = scrollViewsQuery.secureTextFields["passwordTextField"].firstMatch
+        let enterButton = scrollViewsQuery.buttons["enterButton"].firstMatch
+        
+        loginTextField.tap()
+        deleteTextIn(loginTextField)
+        loginTextField.typeText(login)
+        
+        passwordTextField.tap()
+        deleteTextIn(passwordTextField)
+        passwordTextField.typeText(password)
+        
+        enterButton.tap()
+    }
+    
+    func testLoginSuccess() throws {
+        enterAuthorizationData(login: "Test", password: "qwerty123")
+        let catalogNavigationLabel = app.navigationBars["Catalog"].staticTexts["Catalog"].label
+        XCTAssertEqual(catalogNavigationLabel, "Catalog")
+    }
+    
+    func testLoginFail() throws {
+        enterAuthorizationData(login: "admin", password: "admin")
+        app.alerts["Error"].scrollViews.otherElements.buttons["Close"].tap()
     }
 }
